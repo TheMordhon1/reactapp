bd05c41aa352cfde7e9843911ba6da46e90a8c4e
Muhamad Romdon <52556786+TheMordhon1@users.noreply.github.com>
2020-05-15 16:57:24 +0700

    Add files via upload

---
+++ precache-manifest.99aed232ce6ca5f8f448307763f71f6d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4ce82083f7960ca40ce9f904fad50f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "052a681558a5a0b7d1df",
+    "url": "/static/css/main.8bc26537.chunk.css"
+  },
+  {
+    "revision": "4840add7875456f10eb8",
+    "url": "/static/js/2.90b265a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90b265a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052a681558a5a0b7d1df",
+    "url": "/static/js/main.4e3051ad.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "dcdf66da43c65bbd3c168b337ff97239",
+    "url": "/static/media/item1.dcdf66da.jpg"
+  },
+  {
+    "revision": "f52153e14a683c094943e54a4b38e01b",
+    "url": "/static/media/item2.f52153e1.jpg"
+  },
+  {
+    "revision": "b5b80f9a32b3db9c094ea4aac49ff202",
+    "url": "/static/media/item3.b5b80f9a.jpg"
+  },
+  {
+    "revision": "b655abd2e10a4dda4532055aa150af6e",
+    "url": "/static/media/item4.b655abd2.jpg"
+  }
+]);
